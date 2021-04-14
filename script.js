ceab958e548c4caf1472df1ebef9d1974eb511a6
Oded Dugma <odeddugma@gmail.com>
2021-04-14 13:23:28 +0300

    Clean up code at script.js
    Previous version of this code has repeated lines,
    with only few changes between dark and light modes.
    I cleaned up the code, making it more readable and less messy,
    using ternary if operators and Array.forEach() function.

--- script.js
+++ script.js
@@ -1,58 +1,30 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1')
-const image2 = document.getElementById('image2')
-const image3 = document.getElementById('image3')
+const imagesArray = document.querySelectorAll('.imgs')
 const textBox = document.getElementById('text-box');
 
-// Dark/Light Images
-function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+// Change modes styles
+function toggleDarkLightMode(isLight) {
+    let mode = isLight ? 'light' : 'dark';
+    nav.style.backgroundColor = isLight ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = isLight ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    document.documentElement.setAttribute('data-theme', `${mode}`);
+    toggleIcon.children[0].textContent = isLight ? 'Light Mode' : 'Dark Mode';
+    isLight ? toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') : toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imagesArray.forEach(image => image.src = isLight ? image.src.replace('dark', 'light') : image.src.replace('light', 'dark'));
+    localStorage.setItem('theme', mode);
 }
 
-// Dark Mode Style
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
-}
-
-// Light Mode Style
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
-}
-
-// Switch Theme Dynamically
-function switchTheme(event) {
+// Event listener
+toggleSwitch.addEventListener('change', event => {
     if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        darkMode();
+        toggleDarkLightMode(false);
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        lightMode();
+        toggleDarkLightMode(true);
     }
-}
+});
 
-// Event Listener
-toggleSwitch.addEventListener('change', switchTheme);
-
-// Check Local Storage For Theme
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-        darkMode();
-    }
-}
+// Check local storage for theme
+localStorage.getItem('theme') === 'dark' ? toggleDarkLightMode(false) : toggleDarkLightMode(true);
+localStorage.getItem('theme') === 'dark' ? toggleSwitch.checked = true : toggleSwitch.checked = false;
